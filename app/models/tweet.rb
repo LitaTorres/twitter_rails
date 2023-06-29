d36f8a3112d3415f92eaf36a2094dc3689ba6930
Leslie Torres <Leslietorresc@gmail.com>
2023-06-29 02:21:03 -0400

    Prueba Commit 3 buscador listo

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,15 @@ class Tweet < ApplicationRecord
     include PgSearch::Model
 
     pg_search_scope :search_full_text, against: {
-        description: 'A'
-        user_name: 'B'
+        description: 'A',
+        user_name: 'B',
+    },
+
+    using: {
+        tsearch: {
+            prefix: true,
+            any_word: true,
+            dictionary: "spanish",
+        }
     }
 end
